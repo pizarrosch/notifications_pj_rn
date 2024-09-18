ac17eec10d26e70c83b6bf0c6612cdb0b1909d13
pizarrosch <bayernsch89@gmail.com>
2024-09-18 23:21:17 +0200

    Create notifications schedule function (without displaying the notification)

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of notification',
+        data: {name: 'Zaur'}
+      },
+      trigger: {
+        seconds: 5
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Schedule Notification' onPress={scheduleNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
